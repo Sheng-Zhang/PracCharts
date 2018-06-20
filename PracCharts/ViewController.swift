726771e1d4c1c8ca292056ef5e1f3af9cd80fffe
Sheng-Zhang <A-Chang@A-Chang-MacBook-Pro.local>
2018-06-20 19:37:51 +0800

    carthage

--- PracCharts/ViewController.swift
+++ PracCharts/ViewController.swift
@@ -7,12 +7,62 @@
 //
 
 import UIKit
+import Charts
 
 class ViewController: UIViewController {
-
+    var timer: Timer?
+    var dataEntries = [ChartDataEntry]()
+    @IBOutlet weak var scatterChartView: ScatterChartView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        setChart()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true, block: { _ in self.drawChart() })
+        //drawChart()
+        
+    }
+    
+    
+    
+    func setChart() {
+        scatterChartView.scaleXEnabled = false
+        scatterChartView.scaleYEnabled = false
+        scatterChartView.rightAxis.enabled = false
+        
+        scatterChartView.xAxis.setLabelCount(17, force: true)
+        scatterChartView.leftAxis.setLabelCount(17, force: true)
+        
+        scatterChartView.xAxis.gridColor = .black
+        scatterChartView.leftAxis.gridColor = .black
+        
+        
+    }
+    
+    func drawChart() {
+        NSLog("Draw chart!, dataEntries.count: \(dataEntries.count)")
+        //var dataEntries = [ChartDataEntry]()
+        for i in -256...256 {
+            let dataEntry = ChartDataEntry.init(x: Double(i*Int(arc4random())%256), y: Double(i*Int(arc4random())%256))
+            
+            dataEntries.append(dataEntry)
+        }
+        
+        let dataSet = ScatterChartDataSet(values: dataEntries, label: "Sample:")
+        dataSet.scatterShapeSize = 2
+        dataSet.setScatterShape(.circle)
+        dataSet.setColor(.red)
+        
+        let data = ScatterChartData(dataSet: dataSet)
+        
+        scatterChartView.data = data
+        scatterChartView.notifyDataSetChanged()
     }
 
     override func didReceiveMemoryWarning() {
