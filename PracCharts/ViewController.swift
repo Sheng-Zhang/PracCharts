a300cde3ee1d50318b65a12da339138abd259f6b
Sheng-Zhang <ch30529@gmail.com>
2018-06-20 22:30:08 +0800

    Set mas to 15*256.

--- PracCharts/ViewController.swift
+++ PracCharts/ViewController.swift
@@ -12,13 +12,19 @@ import Charts
 class ViewController: UIViewController {
     var timer: Timer?
     var dataEntries = [ChartDataEntry]()
+    let MAX_DATA_NUMBER = 15*256
+    
+    var counter = 0
+    
     @IBOutlet weak var scatterChartView: ScatterChartView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        
+//        for _ in 0..<MAX_DATA_NUMBER {
+//            dataEntries.append(ChartDataEntry(x: 0, y: 0))
+//        }
         
         setChart()
     }
@@ -36,25 +42,41 @@ class ViewController: UIViewController {
         scatterChartView.scaleYEnabled = false
         scatterChartView.rightAxis.enabled = false
         
+        scatterChartView.chartDescription?.enabled = false
+        
+        scatterChartView.xAxis.drawLabelsEnabled = false
+        scatterChartView.leftAxis.drawLabelsEnabled = false
+        
         scatterChartView.xAxis.setLabelCount(17, force: true)
         scatterChartView.leftAxis.setLabelCount(17, force: true)
         
         scatterChartView.xAxis.gridColor = .black
         scatterChartView.leftAxis.gridColor = .black
         
+        scatterChartView.xAxis.axisMaximum = 256
+        scatterChartView.xAxis.axisMinimum = -256
+        
+        scatterChartView.leftAxis.axisMaximum = 256
+        scatterChartView.leftAxis.axisMinimum = -256
         
     }
     
     func drawChart() {
         NSLog("Draw chart!, dataEntries.count: \(dataEntries.count)")
         //var dataEntries = [ChartDataEntry]()
-        for i in -256...256 {
-            let dataEntry = ChartDataEntry.init(x: Double(i*Int(arc4random())%256), y: Double(i*Int(arc4random())%256))
+        for i in -128..<128 {
             
-            dataEntries.append(dataEntry)
+            if MAX_DATA_NUMBER > self.dataEntries.count {
+                dataEntries.append(ChartDataEntry(x: Double(i*Int(arc4random())%256), y: Double(i*Int(arc4random())%256)))
+            } else {
+                
+                dataEntries[counter % MAX_DATA_NUMBER].x = Double(i*Int(arc4random())%256)
+                dataEntries[counter % MAX_DATA_NUMBER].y = Double(i*Int(arc4random())%256)
+            }
+            counter += 1
         }
         
-        let dataSet = ScatterChartDataSet(values: dataEntries, label: "Sample:")
+        let dataSet = ScatterChartDataSet(values: dataEntries, label: "Counter:\(counter)    entry count: \(dataEntries.count)")
         dataSet.scatterShapeSize = 2
         dataSet.setScatterShape(.circle)
         dataSet.setColor(.red)
